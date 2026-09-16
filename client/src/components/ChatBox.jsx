687121def5044f7023d55e04a635c0744b37b751
nishadabhay18 <abhayn927@gmail.com>
2026-09-16 18:54:42 +0530

    check

--- client/src/components/ChatBox.jsx
+++ client/src/components/ChatBox.jsx
@@ -70,7 +70,7 @@ const ChatBox = () => {
   }, [messages])
 
   return (
-    <div className='flex-1 flex flex-col  h-[calc(100vh-50px)] m-5 md:m-10 xl:mx-30 max-md:mt-14 2xl:pr-40'>
+    <div className='flex-1 flex flex-col  h-[calc(100vh-80px)] m-5 md:m-10 xl:mx-30 max-md:mt-14 2xl:pr-40'>
 
       {/* chat messages */}
       {/* <div ref={containerRef} className='flex-1 mb-5 overflow-y-scroll'>
